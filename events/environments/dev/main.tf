fd20a21ff4bf9211bf54ebc6bb711dbbcfae4f04
Tom Haddon <tom.haddon@canonical.com>
2023-01-18 13:55:38 +0100

    initial commit

---
+++ events/environments/dev/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    juju = {
+      source  = "juju/juju"
+      version = "0.4.3"
+    }
+  }
+}
+
+resource "juju_application" "indico" {
+  name  = indico
+  model = indico-test
+
+  charm {
+    name = "indico"
+  }
+}
